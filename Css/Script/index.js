f9e5f3f0b2475be2ecaf38adb5636ab949aa264b
benjamin2orie <ogechukwubenj@gmail.com>
2023-08-22 17:57:07 +0100

    committing changes

---
+++ Css/Script/index.js
@@ -0,0 +1,44 @@
+
+
+// startted writing some logic here
+
+const searchPrize = (e) => {
+//   e.preventDefault();
+
+let array = 'orange';
+switch(array) {
+    case array ='orange':
+        console.log('orange chosed')
+    break;
+    case 'sbanana':
+        console.log('banana chosed')
+    break;
+    case 'srice':
+        console.log('rice chosed')
+    break;
+    case 'ssuger cane':
+        console.log('suger cane chosed')
+    break;
+    default:
+        console.log('no it chosed yet') 
+    break;            
+
+
+}
+    console.log('prizes searched')
+}
+
+// writting some switch statement here
+// const prize = document.querySelector('.input_prize');
+// const number = 6;
+// switch(number) {
+//     case 2:
+//         console.log('the number is 2');
+//         break;
+//     case 3:
+//         console.log('the number is 3');
+//         break;
+//     default:
+//         prize.value = 'no result found'
+//         break;        
+// }
